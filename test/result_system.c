9ee28a44f490e3800432be1813c584f47fa97615
mikfsu <miknstu@gmail.com>
2019-08-20 10:31:04 +0200

    result sustem

---
+++ test/result_system.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main(){
+int i,marks;
+printf("Enter marks:");
+scanf("%d",&marks);
+
+if(marks>=80 && marks<=100){
+    printf("A+",marks);
+}
+
+else if(marks>=70 && marks<=79){
+    printf("A",marks);
+}
+else if(marks>=65 && marks<=69){
+    printf("A-",marks);
+}
+else if(marks>=60 && marks<=64){
+    printf("B",marks);
+}
+else if(marks>=50 && marks<=59){
+    printf("C",marks);
+}
+else if(marks>=40 && marks<=49){
+    printf("D",marks);
+}
+else if(marks>=0 && marks<=39){
+    printf("F",marks);
+}
+else {
+    printf("invaild marks",marks);
+}
+}
